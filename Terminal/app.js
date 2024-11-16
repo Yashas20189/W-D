110c2842751ac495e60e73ad1dcba7ce3a7a902a
Yashas <yashas20189@gmail.com>
2024-11-17 01:25:55 +0530

    adding termial to github

---
+++ Terminal/app.js
@@ -0,0 +1,13 @@
+// git -v -> to get version
+// ls -> list files -> like show my files
+// ~ -> home directory (everthing like desktop and extra is inside home directory)
+// pwd -> print working directory -> where am I?
+// clear 
+// cd -> change directory -> to get in
+// cd .. -> back button in terminal 
+// cd for multiple files example -> cd Desktop/Delta or cd ../.. 
+// mkdir -> used to make new directory 
+// touch -> used to create a file with it's extension eg- index.html
+// delete command very powerful use it carefully 
+// rm -> removes files 
+// rmdir -> romoves only empty folders
