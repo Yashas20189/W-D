0af6af50e5bcf1e1e08586cba9069babf50e9878
Yashas <yashas20189@gmail.com>
2024-11-17 19:33:11 +0530

    Adding new concept - export and process

--- Backend-01/app.js
+++ Backend-01/app.js
@@ -1,7 +1,15 @@
-let n =5;
+// let n =5;
 
-for (let i=0; i<n ; i++){
-    console.log("hello,",i);
-}
+// for (let i=0; i<n ; i++){
+//     console.log("hello,",i);
+// }
 
 // node app.js(name of js) -> we can print output in terminal 
+
+console.log(process.argv);
+
+// process.argv -> used to pass argument (values) to git bash
+
+// module.export
+const math = require("./math");
+console.log(math);
---
+++ Backend-01/math.js
@@ -0,0 +1,5 @@
+// module.export -> is used to export current file data to another file 
+// require -> used to collect the data send by export
+// ./ -> used to mention that file is same folder
+
+module.exports = 123;
